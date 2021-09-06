6bea9bdb548c508ecc357401caeb6fd499e89ba0
7IEH0827 <ys740128@naver.com>
2021-09-06 20:02:27 +0900

    Add C File [1week project]

---
+++ 1주차 개념 확인 과제.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("C프로그래밍1 1주차 과제 \n");
+	printf("이름: 박기태 \n");
+	printf("학번: %d \n", 202111554);
+	return 0;
+}
---
+++ 연습문제 02-02.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("이름: 박기태 \n");
+	printf("주소: 서울특별시 강동구 아리수로93가길 110 \n");
+	printf("전화번호 : 010-5009-6246 \n");
+	return 0;
+}
